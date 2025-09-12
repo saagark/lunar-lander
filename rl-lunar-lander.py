3d4dd73e39c4576f5ceec44d51e9b4cfa0d099d7
saagark <saagarsk@gmail.com>
2025-09-11 23:40:10 -0700

    add transition tuple, replay buffer, dqn module

--- rl-lunar-lander.py
+++ rl-lunar-lander.py
@@ -5,35 +5,60 @@ import torch.nn as nn
 import torch.nn.functional as F
 from collections import deque, namedtuple
 import time
+import random
 
+Transition = namedtuple(
+    "Transition",
+    ("state",
+     "action",
+     "reward",
+     "next_state",
+     "done"))
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        """Save a transition"""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
 
 def DQN(nn.Module):
     def __init__(self, state_size, action_size, seed):
         super(DQN, self).__init__()
         self.seed = torch.manual_seed(seed)
-        self.fc1 = nn.Linear(state_size, 64)
-        self.fc2 = nn.Linear(64, 64)
-        self.fc3 = nn.Linear(64, action_size)
+        self.net = nn.Sequential(
+            nn.Linear(state_size, 64),
+            nn.ReLU(),
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, action_size)
+        )
 
-    def forward(self, state):
-        x = F.relu(self.fc1(state))
-        x = F.relu(self.fc2(x))
-        return self.fc3(x)
+    def forward(self, x):
+        return self.net(x)
+
 
 def main():
     env = gym.make("LunarLander-v3", continuous=False, gravity=-10.0,
-               enable_wind=False, wind_power=15.0, turbulence_power=1.5)
+                   enable_wind=False, wind_power=15.0, turbulence_power=1.5)
+
 
 if __name__ == '__main__':
     device = torch.device(
-    "cuda" if torch.cuda.is_available() else
-    "mps" if torch.backends.mps.is_available() else
-    "cpu"
+        "cuda" if torch.cuda.is_available() else
+        "mps" if torch.backends.mps.is_available() else
+        "cpu"
     )
-    
+
     t = time.time()
     main()
     elapsed = time.time() - t
     mins, secs = divmod(elapsed, 60)
     print(f"Execution time: {int(mins)} min {secs:.2f} sec")
-
